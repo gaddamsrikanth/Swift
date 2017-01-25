0660565de577067308028f3bf9ce9c616147fe23
gaddamsrikanth <gaddam.srikanth.gollapally@gmail.com>
2017-01-25 16:02:17 +0530

    Pushing from one window to other

--- sampleproject/sampleproject/ViewController.swift
+++ sampleproject/sampleproject/ViewController.swift
@@ -10,16 +10,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var OutButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
+    @IBAction func change(_ sender: UIButton!) {
+        let vc = SecondViewController()
+        self.navigationController?.pushViewController(vc, animated: true)
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
+}
