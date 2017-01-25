daeeafebe6f178daba7fa72631ff59eef1438827
gaddamsrikanth <gaddam.srikanth.gollapally@gmail.com>
2017-01-25 11:16:50 +0530

    New Swift Project

---
+++ swift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  swift
+//
+//  Created by Tilak on 25/01/17.
+//  Copyright © 2017 LaNet. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
