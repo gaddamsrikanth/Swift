1aa08865978c16a950be2e62aa30a78535eec599
gaddamsrikanth <gaddam.srikanth.gollapally@gmail.com>
2017-01-27 13:26:06 +0530

    Table View with dynamic and static addition of data

--- sampleproject/sampleproject/ViewController.swift
+++ sampleproject/sampleproject/ViewController.swift
@@ -8,15 +8,41 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController:UIViewController,UITableViewDelegate,UITableViewDataSource {
+    
+    
+    var animals : [String] = ["A","B","C","D"]
+    let cellReuseIdentifier = "cell"
+    
+    
+    
+    @IBOutlet var add: UIButton!
+    @IBOutlet var tableView: UITableView!
     @IBOutlet var textField: UITextField!
     @IBOutlet var lbl: UILabel!
     @IBOutlet var OutButton: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.tableView.register(UITableViewCell.self,forCellReuseIdentifier : cellReuseIdentifier)
+        tableView.delegate = self
+        tableView.dataSource = self
         // Do any additional setup after loading the view, typically from a nib.
     }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.animals.count
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell:UITableViewCell = self.tableView.dequeueReusableCell(withIdentifier: cellReuseIdentifier) as UITableViewCell!
+        cell.textLabel?.text = self.animals[indexPath.row]
+        return cell
+    }
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("You tapped cell number \(indexPath.row)")
+    }
+    
+    
     
     @IBAction func txt(_ sender: Any) {
         lbl.text = textField.text
@@ -26,6 +52,12 @@ class ViewController: UIViewController {
         vc.myString = textField.text
         self.navigationController?.pushViewController(vc, animated: true)
     }
+    
+    @IBAction func added(_ sender: Any) {
+        if(textField.text != ""){
+        animals.append(textField.text!)
+            tableView.reloadData()
+        }}
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
