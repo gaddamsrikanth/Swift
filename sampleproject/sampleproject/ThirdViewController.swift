c63fa136463a0e68855355dd6623b2458cb99b47
gaddamsrikanth <gaddam.srikanth.gollapally@gmail.com>
2017-01-28 18:31:45 +0530

    Adding image from photo library to uicollectionview

--- sampleproject/sampleproject/ThirdViewController.swift
+++ sampleproject/sampleproject/ThirdViewController.swift
@@ -7,14 +7,31 @@
 //
 
 import UIKit
+import Foundation
+import Photos
+import PhotosUI
 
 class ThirdViewController: UIViewController,UICollectionViewDataSource,UICollectionViewDelegateFlowLayout {
-
+    let identifier = "CollectionViewCell"
+    var image = UIImage(named: "1")
     @IBOutlet var v1: UIView!
+    var imageView: UIImageView!
     @IBOutlet var collectionView: UICollectionView!
     @IBOutlet var orgn: UIButton!
+    
+    fileprivate let imageManager = PHCachingImageManager()
+    var fetchResult: PHFetchResult<PHAsset>!
+    var assetCollection: PHAssetCollection!
+    fileprivate var thumbnailSize: CGSize!
+    fileprivate var previousPreheatRect = CGRect.zero
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        let allPhotosOptions = PHFetchOptions()
+        allPhotosOptions.sortDescriptors = [NSSortDescriptor(key: "creationDate", ascending: true)]
+        fetchResult = PHAsset.fetchAssets(with: allPhotosOptions)
+        PHPhotoLibrary.shared().register(self)
+        
         let layout: UICollectionViewFlowLayout = UICollectionViewFlowLayout()
         layout.sectionInset = UIEdgeInsets(top: 10, left: 10, bottom: 10, right: 25)
         layout.itemSize = CGSize(width: 100, height: 100)
@@ -22,10 +39,13 @@ class ThirdViewController: UIViewController,UICollectionViewDataSource,UICollect
         collectionView = UICollectionView(frame: self.v1.frame, collectionViewLayout: layout)
         collectionView.dataSource = self
         collectionView.delegate = self
-        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "Cell")
+        collectionView.register(UINib(nibName:"CollectionViewCell", bundle: nil), forCellWithReuseIdentifier: "CollectionViewCell")
         collectionView.backgroundColor = UIColor.clear
         self.v1.addSubview(collectionView)
         // Do any additional setup after loading the view.
+    }
+    deinit {
+        PHPhotoLibrary.shared().unregisterChangeObserver(self)
     }
 
     @IBAction func orgn1(_ sender: UIButton) {
@@ -38,16 +58,34 @@ class ThirdViewController: UIViewController,UICollectionViewDataSource,UICollect
         // Dispose of any resources that can be recreated.
     }
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 21
+        return self.fetchResult.count
     }
     
-    internal func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath as IndexPath)
-        cell.backgroundColor = UIColor.red
+    
+    fileprivate func resetCachedAssets() {
+        imageManager.stopCachingImagesForAllAssets()
+        previousPreheatRect = .zero
+    }
+    
+   internal func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+    guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CollectionViewCell", for: indexPath) as? CollectionViewCell
+        else{fatalError("no cell")}
+    let asset = fetchResult.object(at: indexPath.item)
+    let img = getAssetThumbnail(asset: asset)
+    cell.img.image = img
         return cell
     }
-
-    /*
+    func getAssetThumbnail(asset: PHAsset) -> UIImage {
+        let manager = PHImageManager.default()
+        let option = PHImageRequestOptions()
+        var thumbnail = UIImage()
+        option.isSynchronous = true
+        manager.requestImage(for: asset, targetSize: CGSize(width: 100, height: 100), contentMode: .aspectFit, options: option, resultHandler: {(result, info)->Void in
+            thumbnail = result!
+        })
+        return thumbnail
+    }
+            /*
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
@@ -57,4 +95,44 @@ class ThirdViewController: UIViewController,UICollectionViewDataSource,UICollect
     }
     */
 
+}
+// MARK: PHPhotoLibraryChangeObserver
+extension ThirdViewController: PHPhotoLibraryChangeObserver {
+    func photoLibraryDidChange(_ changeInstance: PHChange) {
+        
+        guard let changes = changeInstance.changeDetails(for: fetchResult)
+            else { return }
+        
+        // Change notifications may be made on a background queue. Re-dispatch to the
+        // main queue before acting on the change as we'll be updating the UI.
+        DispatchQueue.main.sync {
+            // Hang on to the new fetch result.
+            fetchResult = changes.fetchResultAfterChanges
+            if changes.hasIncrementalChanges {
+                // If we have incremental diffs, animate them in the collection view.
+                guard let collectionView = self.collectionView else { fatalError() }
+                collectionView.performBatchUpdates({
+                    // For indexes to make sense, updates must be in this order:
+                    // delete, insert, reload, move
+                    if let removed = changes.removedIndexes, removed.count > 0 {
+                        collectionView.deleteItems(at: removed.map({ IndexPath(item: $0, section: 0) }))
+                    }
+                    if let inserted = changes.insertedIndexes, inserted.count > 0 {
+                        collectionView.insertItems(at: inserted.map({ IndexPath(item: $0, section: 0) }))
+                    }
+                    if let changed = changes.changedIndexes, changed.count > 0 {
+                        collectionView.reloadItems(at: changed.map({ IndexPath(item: $0, section: 0) }))
+                    }
+                    changes.enumerateMoves { fromIndex, toIndex in
+                        collectionView.moveItem(at: IndexPath(item: fromIndex, section: 0),
+                                                to: IndexPath(item: toIndex, section: 0))
+                    }
+                })
+            } else {
+                // Reload the collection view if incremental diffs are not available.
+                collectionView!.reloadData()
+            }
+            resetCachedAssets()
+        }
+    }
 }
