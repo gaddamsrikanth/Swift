255a9cd1f2c8151b4901bc47036222d5312e0791
gaddamsrikanth <gaddam.srikanth.gollapally@gmail.com>
2017-02-16 12:31:05 +0530

    Route between two location

--- sampleproject/sampleproject/TabViewController2.swift
+++ sampleproject/sampleproject/TabViewController2.swift
@@ -8,16 +8,23 @@
 import MapKit
 import UIKit
 
-class TabViewController2: UIViewController,CLLocationManagerDelegate {
+class TabViewController2: UIViewController,CLLocationManagerDelegate, MKMapViewDelegate {
+    var myRoute : MKRoute!
+    var directionsRequest = MKDirectionsRequest()
+
     var a : CLLocationCoordinate2D!
     var b : CLLocationCoordinate2D!
     let locationmanager = CLLocationManager()
     @IBOutlet var map: MKMapView!
     var matchingItems: [MKMapItem] = [MKMapItem]()
-    
+    var cord : [CLLocationCoordinate2D] = []
     @IBOutlet var textFeild: UITextField!
         override func viewDidLoad() {
             super.viewDidLoad()
+            map.delegate = self
+            let point1 = MKPointAnnotation()
+            let point2 = MKPointAnnotation()
+          
             locationmanager.delegate = self
             locationmanager.desiredAccuracy = kCLLocationAccuracyBest
             locationmanager.requestWhenInUseAuthorization()
@@ -25,15 +32,50 @@ class TabViewController2: UIViewController,CLLocationManagerDelegate {
             
             let a = locationmanager.location!.coordinate.latitude
             let b = locationmanager.location!.coordinate.longitude
-            print(a)
-            print(b)
             let locationOne = CLLocationCoordinate2DMake(a, b)
-            let annotation = MKPointAnnotation()
-            map.removeAnnotation(annotation as MKAnnotation)
-            annotation.coordinate = locationOne
-            map.addAnnotation(annotation as MKAnnotation)
+            point1.coordinate = locationOne
+            point1.title = "Mumbai"
+            map.addAnnotation(point1)
             
-        }
+            point2.coordinate = CLLocationCoordinate2DMake(40.063700, -75.646277)
+            point2.title = "Surat"
+            map.addAnnotation(point2)
+            map.centerCoordinate = point2.coordinate
+
+            let mumbai = MKPlacemark(coordinate: CLLocationCoordinate2DMake(point1.coordinate.latitude, point1.coordinate.longitude), addressDictionary: nil)
+            
+            let surat = MKPlacemark(coordinate: CLLocationCoordinate2DMake(point2.coordinate.latitude, point2.coordinate.longitude), addressDictionary: nil)
+            
+            map.setRegion(MKCoordinateRegionMake(point1.coordinate, MKCoordinateSpanMake(0.7,0.7)), animated: true)
+            directionsRequest.source = MKMapItem(placemark: mumbai)
+            directionsRequest.destination = MKMapItem(placemark: surat)
+            
+            directionsRequest.transportType = MKDirectionsTransportType.automobile
+            let directions = MKDirections(request: directionsRequest)
+            
+            directions.calculate(completionHandler: {
+                response, error in
+                
+                if error == nil {
+                    self.myRoute = response!.routes[0] as MKRoute
+                    print("Route")
+                    self.map.add(self.myRoute.polyline)
+                }
+                else{
+                print("Error")
+                }
+                
+            })
+    }
+    
+    func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
+        
+        let myLineRenderer = MKPolylineRenderer(polyline: myRoute.polyline)
+        myLineRenderer.strokeColor = UIColor.red
+        myLineRenderer.lineWidth = 3
+        return myLineRenderer
+    }
+            
     
     func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
         if status == .authorizedWhenInUse
@@ -47,11 +89,6 @@ class TabViewController2: UIViewController,CLLocationManagerDelegate {
     }
     
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        if let location = locations.first{
-        let span = MKCoordinateSpanMake(0.5, 0.5)
-        let region = MKCoordinateRegionMake(location.coordinate, span)
-            map.setRegion(region, animated: true)
-        }
         
     }
     
@@ -96,12 +133,10 @@ class TabViewController2: UIViewController,CLLocationManagerDelegate {
                     
                     self.matchingItems.append(item as MKMapItem)
                     print("Matching items = \(self.matchingItems.count)")
-                    
                     let annotation = MKPointAnnotation()
                     annotation.coordinate = item.placemark.coordinate
                     annotation.title = item.name!
-                    print(annotation.coordinate)
-                    self.map.addAnnotation(annotation)
+                self.map.addAnnotation(annotation)
                 }
             }
         })
